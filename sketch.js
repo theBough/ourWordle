b8be7682ca159c90a0494c6e6e57c28fadee5d8a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-03-09 11:54:59 -0600

    Made a 5 by 6 grid

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 let b = [];
 function setup() {
   createCanvas(400, 400);
-  for(var i =0 ; i<6 ; i++){
+  for(var i =0 ; i<5 ; i++){
     b[i] = [];
-    for(var j=0 ; j<5; j++){
-      b[i][j] = new Box(20,20,50,50);
+    for(var j=0 ; j<6; j++){
+      b[i][j] = new Box(20 + i *55,20+j*55,50,50);
     }//end inside ForLoop
   }//end for loop
 }//end Setup
 
 function draw() {
   background(220);
-  for(var i =0 ; i<6 ; i++){
-    for(var j=0 ; j<5 ; j++){
+  for(var i =0 ; i<5 ; i++){
+    for(var j=0 ; j<6 ; j++){
       b[i][j].display()
     }
   }//end for loop
