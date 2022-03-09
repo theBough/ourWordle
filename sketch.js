6bcd65b1b6c88f535a0ef3d885e0968d2ecaec56
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-03-09 11:51:35 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,20 @@
 let b = [];
 function setup() {
   createCanvas(400, 400);
-  for(var i =0 ; i<5 ; i++){
-    b.push(new Box(10+i*55,200,50,50));
+  for(var i =0 ; i<6 ; i++){
+    b[i] = [];
+    for(var j=0 ; j<5; j++){
+      b[i][j] = new Box(20,20,50,50);
+    }//end inside ForLoop
   }//end for loop
-  
-}
+}//end Setup
 
 function draw() {
   background(220);
-  for(var i =0 ; i<5 ; i++){
-    b[i].display();
+  for(var i =0 ; i<6 ; i++){
+    for(var j=0 ; j<5 ; j++){
+      b[i][j].display()
+    }
   }//end for loop
   
   textSize(20)
