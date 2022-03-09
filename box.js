93c5dcd68b4f3893ca82b920a7b3521aeb4232ee
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-03-09 12:01:50 -0600

    Added a letter

--- box.js
+++ box.js
@@ -7,5 +7,7 @@ function Box(x,y,w,h,letter){
   
   this.display = function(){
     rect(this.x, this.y, this.w, this.h);
+    textSize(26);
+    text(this.letter, this.x +5, this.y + 5)
   }//end display
 }//end Box
