376d3a9467a49443c28c7a799db62e81cb6ed9b3
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-03-16 12:17:31 -0500

    Added a Word list database
    Also made the keyboard presses show in the boxes.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,27 @@
 let b = [];
+let words = [];
+/*
+these two variables will keep track of 
+where the current guess letter is.
+*/
+let activeX, activeY
+
+function preload(){
+  /*loadString() will load words
+  from a file and RETURN an array 
+  of words. Make sure you have a file in 
+  your directory.
+  */
+  words = loadStrings("list.txt");
+}
 function setup() {
   createCanvas(400, 400);
+  activeX =0;
+  activeY = 0;  
   for(var i =0 ; i<5 ; i++){
     b[i] = [];
     for(var j=0 ; j<6; j++){
-      b[i][j] = new Box(20 + i *55,20+j*55,50,50);
+      b[i][j] = new Box(20 + i *55,20+j*55,50,50,"");
     }//end inside ForLoop
   }//end for loop
 }//end Setup
@@ -21,5 +38,6 @@ function keyReleased(){
   /*this function gets called automatically
   everytime a key on the keyboard is released
   */
-  b[0][0].letter = key;
+  b[activeX][activeY].letter = key;
+  activeX += 1
 }//end keyReleased
