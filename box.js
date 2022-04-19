1225e2989949accc09f218f71daa3ab03f62abe9
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-19 11:27:43 -0500

    You can change a colour now

--- box.js
+++ box.js
@@ -1,13 +1,16 @@
-function Box(x,y,w,h,letter){
+function Box(x,y,w,h,letter,col){
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   this.letter = letter;
+  this.col = col
   
   this.display = function(){
+    fill(this.col)
     rect(this.x, this.y, this.w, this.h);
     textSize(26);
+    fill(0)
     text(this.letter, this.x +15, this.y + 30)
   }//end display
 }//end Box
