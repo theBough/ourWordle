fa211b10d034145d86516f87c4c0dc3debc377d1
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-26 11:33:43 -0500

    Three rows of keys

--- keyboard.js
+++ keyboard.js
@@ -1,9 +1,18 @@
-let k = []
+let k = [];
+let order = [
+  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+  ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+  ["Z", "X", "C", "V", "B", "N", "M"],
+];
 
-function makeKeyboard(){
-  for(i = 0 ; i<10 ; i++){
-    k[i] = new Clickable();
-    k[i].locate(20 + i * 25,400)
-  }
-  
-}//end makeKeyboard
+function makeKeyboard() {
+  for (j = 0; j < 3; j++) {
+    k[j] = []
+    for (i = 0; i < 10; i++) {
+      k[j][i] = new Clickable();
+      k[j][i].locate(20 + i * 25, 350 + j*55);
+      k[j][i].width = 24;
+      k[j][i].heigth = 50;
+    } //end i loop
+  } //end j loop
+} //end makeKeyboard
